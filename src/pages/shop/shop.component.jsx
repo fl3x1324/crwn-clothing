e959d166c9493bd2591006544ab13e503b976ed0
Atanas Minkov <atanas.minkov@icloud.com>
2021-04-11 13:08:34 +0300

    Update: Shop Page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import { SHOP_DATA } from "../../data/shop";
 
 class ShopPage extends React.Component {
@@ -10,9 +11,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
+    const { collections } = this.state;
     return (
-      <div>
-        <h1>SHOP PAGE</h1>
+      <div className={"shop-page"}>
+        {collections
+          .filter((_, idx) => idx < 4)
+          .map(({ id, ...rest }) => (
+            <CollectionPreview key={id} {...rest} />
+          ))}
       </div>
     );
   }
